1a21d8016e6c8f35ce2eefccb12f2dfd1ee72f47
Ben Simpson <b.albertsimpson@gmail.com>
2020-03-03 17:30:11 -0800

    building signup and login backend

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path, include
+
+urlpatterns = [
+    path('SignUp/', views.SignUpView.as_view()),
+
+]
---
+++ accounts/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from accounts.forms import UserCreateForm
+from django.contrib import messages
+from accounts.models import Profile
+from django.views.generic.list import ListView
+from django.contrib.auth.models import User
+
+
+
+class SignUpView(CreateView):
+    
+    
+    # def get(self, request):
+  form_class = UserCreateForm
+  success_url = reverse_lazy('login')
+  
+  # This works because using the CreatView and using template name with it to show the page
+  template_name = 'registration/signup.html'
+        # return render(request, template_name, {
+        #   'form': form_class
+        # })
+
+
+class Sayhi(CreateView):
+  def get(self, request):
+      return render(request, "base.html")
+
+class Homepage(CreateView):
+    def get(self, request):
+        return render(request, "accounts/home.html")
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+# def update_profile(request, user_id):
+#     user = User.objects.get(pk=user_id)
+#     user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
+#     user.save()
+
+def update_profile(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, ('Your profile was successfully updated!'))
+            return redirect('list-rides-page')
+        else:
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'accounts/profile.html', {
+        'profile_form': profile_form
+    })
+
+class ProfilesListView(ListView):
+
+    def get(self, request):
+        riders = User.objects.filter(profile__is_passenger=False)
+        return render(request, 'accounts/profile_list.html', {'profiles': riders})
+
+class GetRiders(CreateView):
+
+    def get(self, request):
+        riders = User.objects.filter(profile__is_passenger=True)
+        return render(request, 'accounts/riders.html', {'riders': riders})
+
+    def login_view_profile(request):
+        user = request.user
+        args = {'user': user}
+        return render(request, 'accounts/account.html', args)
+
+class ConfirmPage(CreateView):
+
+    def get(self, request):
+        return render(request, 'accounts/request_confirm.html')
+# Create your views here.
